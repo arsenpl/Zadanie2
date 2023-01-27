fbdca64bbc93ce514ea1ab4f6eff79ea0b0a58af
root <root@DESKTOP-40D8MAA.localdomain>
2023-01-27 17:52:33 +0100

    changes

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -5,7 +5,7 @@ export default () => {
   return (
     <div>
       <h2>Multifib</h2>
-      <h2>Arsen Plaksyvyi</h2>
+      <h2>Arsen Plaksyvyisa</h2>
       <h2>Grupa 2.3</h2>
       <Link to="/">Go back home</Link>
     </div>
